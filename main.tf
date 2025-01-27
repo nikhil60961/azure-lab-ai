5952b7ba94f5c4842766f505b1c7d6048fd841c3
Nikhil <nikhil.r@qburst.com>
2025-01-27 17:21:25 +0530

    terraform apply dev

--- main.tf
+++ main.tf
@@ -1,10 +1,27 @@
-resource "azurerm_public_ip" "example" {
-  name                = "ai-${var.environment}"
-  resource_group_name = var.rg
-  location            = var.location
-  allocation_method   = "Static"
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg
+  location = var.location
+}
 
-  tags = {
-    environment = "${var.environment}"
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfstate${random_string.random.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  lifecycle {
+    prevent_destroy = true
   }
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "tfstate-aifoundry-dev"
+  storage_account_id    = azurerm_storage_account.storage.id
+  container_access_type = "private"
+}
+
+resource "random_string" "random" {
+  length  = 6
+  special = false
 }
